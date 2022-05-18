b8fb9e269936d81bdbd672efa1b40b1a4a5b0c0c
Ob <oblidi17@gmail.com>
2022-05-18 12:28:30 +0100

    minesweeper message

---
+++ src/main/java/Main.java
@@ -0,0 +1,28 @@
+public class Main {
+    public static void main(String[] args) {
+
+        boolean B = true;
+        System.out.println(B);
+
+
+        String name = "obaida";
+        name = "johnny"; // name ahs changed to johnny
+
+        System.out.println( "Hello" + name);
+
+        String firstName = "ob";
+        String lastName = "Blidi";
+        String fullName = (firstName + lastName);
+
+        System.out.println(fullName);
+
+        int x = 15, y = 16, z=13;
+        System.out.println(x + y + z);
+
+
+
+
+
+
+    }
+}
